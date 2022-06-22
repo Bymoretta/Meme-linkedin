3041d7d344a7cac1d1a8da4ec2f984d7f4782246
Bymoretta <axelmoretta22@gmail.com>
2022-06-21 23:14:29 -0300

    Meme

---
+++ meme.js
@@ -0,0 +1,6 @@
+function developerInProgress(nombre, edad){ 
+    this.nombre = nombre 
+    this.edad = edad 
+    this.buenasPracticas = null 
+    }
+    iam = new  developerInProgress ("Axel Moretta",21) 
